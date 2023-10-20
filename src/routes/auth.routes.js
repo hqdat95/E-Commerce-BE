c3049c9e55d6e7fb1c7baab4fe83ecdea63ff445
hqdat95 <hqdat.95vn@gmail.com>
2023-10-20 22:54:56 +0700

    Local login feature

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 
-import { user } from '../validators';
+import { user, login } from '../validators';
 import { async, validate } from '../middlewares';
-import { registerCtrl } from '../controllers/auth.controller';
+import { registerCtrl, loginCtrl } from '../controllers/auth.controller';
 
 const router = Router();
 
 router.post('/register', validate(user), async(registerCtrl));
+
+router.post('/login', validate(login), async(loginCtrl));
 
 export default router;
